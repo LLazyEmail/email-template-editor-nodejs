61346aac8c5dc2c81e26de57bceaf35392225140
Vadim <vadim.putrov@gmail.com>
2023-03-12 12:23:22 +0200

    add deletion of tree node

--- app.js
+++ app.js
@@ -86,6 +86,44 @@ app.post("/update-node-tree", async (req, res) => {
   res.status(200).send();
 });
 
+const deleteItem = (itemKey, treeData1) => {
+  const findAndDeleteItem = (treeData) => {
+    const foundElement = treeData.find((item) => {
+      if (item.key === itemKey) {
+        return true;
+      }
+
+      if (!item.children) {
+        return false;
+      }
+
+      return findAndDeleteItem(item.children);
+    });
+
+    if (foundElement) {
+      console.log("found", foundElement);
+      const index = treeData.indexOf(foundElement);
+      if (index > -1) {
+        treeData.splice(index, 1);
+      }
+    }
+  };
+
+  findAndDeleteItem(treeData1);
+
+  return treeData1;
+};
+
+app.post("/delete-node-tree", async (req, res) => {
+  await db.read();
+
+  const modified = deleteItem(req.body.key, db.data.recipeTemplate);
+  db.data.recipeTemplate = modified;
+
+  await db.write();
+  res.status(200).send();
+});
+
 const addNodeTree = (itemToAdd1, treeData1, parentKey) => {
   const findAndAddItem = (treeData) => {
     treeData.forEach((item) => {
